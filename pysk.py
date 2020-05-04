c259e94c8616156bcd6708e2ada3d594239e8717
NessMC <contact@nessmc.fr>
2020-05-04 16:00:44 +0200

    ✨ File class added.

--- pysk.py
+++ pysk.py
@@ -8,5 +8,24 @@
 import re
 import unidecode
 
-class FRS:
-    pass
+class FRS: 
+
+    def __init__ (self):
+        pass
+    
+    class File:
+        
+        def __init__ (self):
+            pass
+
+        def ask (self):
+            answer = input('- Fichier à transpiler > ')
+            return answer
+
+        def file (self):
+            input = self.ask()
+            try:
+                file = open(input, encoding='UTF8')
+            except:
+                print('Une erreur est survenue durant la lecture du fichier.')
+            return file.read()
